ed175befcd72cb097ec5e136e26d03cb02d9a3c9
javiercp6 <javiercp@estudiantes.uci.cu>
2023-11-26 15:33:04 -0500

    add icon empty car

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -31,6 +31,30 @@
               />
               <div v-if="store.cart.length !== index + 1" class="divider"></div>
             </div>
+            <div v-if="!store.cart.length">
+              <div class="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-28 h-28"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                  <path d="M17 17a2 2 0 1 0 2 2" />
+                  <path d="M17 17h-11v-11" />
+                  <path d="M9.239 5.231l10.761 .769l-1 7h-2m-4 0h-7" />
+                  <path d="M3 3l18 18" />
+                </svg>
+              </div>
+              <div class="text-center">
+                <p>Empty car</p>
+              </div>
+            </div>
           </div>
         </div>
 
